c59600baaadf82feaa8a2ce5dee508f415f1cf20
ilaxmidhar <idrishlaxmidhar@gmail.com>
2015-04-04 03:23:58 -0400

    Logout changes

--- server/logout.php
+++ server/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 session_destroy();   // function that Destroys Session 
-//header("Location: http://localhost/dailyreport/login.html");
-header("Location: http://localhost:8080/citrix-dr/login.html");
+header("Location: http://localhost/dailyreport/login.html");
+//header("Location: http://localhost:8080/citrix-dr/login.html");
 exit;
 ?>
